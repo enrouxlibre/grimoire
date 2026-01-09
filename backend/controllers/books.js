b60a6a23b83ea61ba28415c08edc585788041d51
enrouxlibre <enrouxlibre23@gmail.com>
2026-01-09 14:42:45 +0100

    Fixed unauthorized error

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -51,7 +51,7 @@ exports.modifyBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then((book) => {
       if (book.userId != req.auth.userId) {
-        res.status(401).json({ message: "Not authorized" });
+        res.status(403).json({ message: "Unauthorized request" });
       } else {
         Book.updateOne(
           { _id: req.params.id },
@@ -70,7 +70,7 @@ exports.deleteBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then((book) => {
       if (book.userId != req.auth.userId) {
-        res.status(401).json({ message: "Ceci n'est pas votre livre !" });
+        res.status(403).json({ message: "Unauthorized request" });
       } else {
         const filename = book.imageUrl.split("/images/")[1];
         fs.unlink(`images/${filename}`, () => {
